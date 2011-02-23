f530c87d52e63642089c138286f8be4ddcfade3b
Nuno Santos <nunofvsantos@gmail.com>
2011-02-23 16:24:52 +0000

    changed entities index to display subscriptions / affiliations count

--- pubsub/views/entities.py
+++ pubsub/views/entities.py
@@ -1,39 +1,33 @@
 from django.shortcuts import render_to_response
+from django.db.models import Count
 from django.core.paginator import Paginator, InvalidPage, EmptyPage
 from django.core.exceptions import ObjectDoesNotExist
 from django.template import RequestContext
 
-from pubsub.models import Entity, Subscription
+from pubsub.models import Entity, Subscription, Affiliation
 
 def index(request):
 
-    entities = Entity.objects.all()
+    entities = Entity.objects.annotate(
+            subscriptions_count=Count('subscription', distinct=True)).annotate(
+            affiliations_count=Count('affiliation', distinct=True)).order_by(
+            'jid')
+
     return render_to_response('entities/index.html',
                               {'entities': entities},
                               context_instance=RequestContext(request))
 
 def details(request, entity_id):
-    
-    entity = Entity.objects.get(pk=entity_id)
-    
-    subscriptions_list = Subscription.objects.filter(entity__entity_id=entity_id)
-    
-    # paginate subscriptions
-    subscriptions = Paginator(subscriptions_list, 20)
-    request_page = request.GET.get('page', '1')
-    try:
-        page = int(request_page)
-    except ValueError:
-        if isinstance(request_page, basestring) and request_page == 'last':
-            page = subscriptions.num_pages
-        else:
-            page = 1
 
-    try:
-        subscriptions = subscriptions.page(page)
-    except (EmptyPage, InvalidPage):
-        subscriptions = subscriptions.page(subscriptions.num_pages)
-    
+    entity = Entity.objects.select_related().get(pk=entity_id)
+
+    subs_count = Subscription.objects.filter(
+            entity__entity_id=entity_id).count()
+
+    affs_count = Affiliation.objects.filter(
+            entity__entity_id=entity_id).count()
+
     return render_to_response('entities/details.html',
-                              {'entity': entity,
-                               'paginator': subscriptions})
+            {'entity': entity,
+             'subscriptions_count': subs_count,
+             'affiliations_count': affs_count})
